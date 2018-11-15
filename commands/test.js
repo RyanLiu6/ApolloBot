3a20929896772ff103b641dc61a3c1427c4712dd
Ryan Liu <dio.ryanliu@hotmail.com>
2018-11-15 12:06:55 -0800

    fixed bugs in secret santa command

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,4 @@
 // Testing for PM
 exports.run = (client, message, args) => {
     message.author.send("testing").catch(console.error);
-
-    let user = client.users.find(user => user.username == client.config.owner);
-    user.send("does this work?");
 }
